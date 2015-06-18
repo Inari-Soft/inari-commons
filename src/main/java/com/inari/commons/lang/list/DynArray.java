37156947b261309a65fea8b4b0ce2937c9dfc8f3
Inari Soft <InariSoft@yahoo.de>
2015-06-18 11:38:42 +0200

    Update DynArray.java

--- src/main/java/com/inari/commons/lang/list/DynArray.java
+++ src/main/java/com/inari/commons/lang/list/DynArray.java
@@ -20,18 +20,6 @@
 * . Neither the name "InariUtils" nor the names of its contributors may be
 * used to endorse or promote products derived from this software without
 * specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
 */
 package com.inari.commons.lang.list;
 
@@ -41,22 +29,23 @@ import java.util.Iterator;
 /** The advantages of ArrayList and an Array in one.
  *  Use DynArray if a good performance matters and just a simple indexed storage is used to set and get objects from.
  *  
- *  Internally this is implemented within an ArrayList but with fixed positions (indices) of stored objects like an Array. An ArrayList
- *  has good performance on get and add calls but not an add( index ) and remove( index ) calls because of the index shift that is needed
- *  and ArrayList has no good performance on iteration because there are a couple of concurrent modification checks done while ArrayList iteration.
+ *  Internally this is implemented within an ArrayList but with fixed positions (indices) of stored objects like 
+ *  an Array. An ArrayList has good performance on get and add calls but not an add( index ) and remove( index ) 
+ *  calls because of the index shift that is needed and ArrayList has no good performance on iteration because 
+ *  there are a couple of concurrent modification checks done while ArrayList iteration.
  *  
- *  DynArray has no add methods but set( index ) method like an array and if there is already an object referenced by this index, the referenced
- *  is overwritten with the new object reference like in an array. 
- *  The remove( index ) method use first a get of the already referenced object and then set a null value at the specified position to avoid
- *  index shifting.
+ *  DynArray has no add methods but set( index ) method like an array and if there is already an object referenced 
+ *  by this index, the referenced is overwritten with the new object reference like in an array. 
+ *  The remove( index ) method use first a get of the already referenced object and then set a null value at the 
+ *  specified position to avoid index shifting.
  *  
- *  DynArray uses an Iterator implementation using index without concurrent modification checks and is as fast as an array iteration.
- *  But DynArray is not synchronized and do not check concurrent modifications in any case.
+ *  DynArray uses an Iterator implementation using index without concurrent modification checks and is as fast as 
+ *  an array iteration. But DynArray is not synchronized and do not check concurrent modifications in any case.
  *  
- *  If the index to set is higher as the current capacity of the internal ArrayList the internal ArrayList is been automatically growing
- *  like a normal ArrayList and filled with null values to fit the new capacity.
- *  The capacity of the list (and the internal ArrayList) never shrink while just remove objects. Only the clear functions removes all objects and
- *  also clears the internal ArrayList.
+ *  If the index to set is higher as the current capacity of the internal ArrayList the internal ArrayList is 
+ *  been automatically growing like a normal ArrayList and filled with null values to fit the new capacity.
+ *  The capacity of the list (and the internal ArrayList) never shrink while just remove objects. Only the clear 
+ *  functions removes all objects and also clears the internal ArrayList.
  *
  * @param <T> The type of objects in the DynArray
  */
@@ -70,8 +59,8 @@ public final class DynArray<T> implements Iterable<T> {
         list = new ArrayList<T>();
     }
     
-    /** Creates a DynArray with specified initial capacity. The internal ArrayList is initialized with initialCapacity + ( initialCapacity / 2 )
-     *  and filled up with null values for the indexes to initialCapacity.
+    /** Creates a DynArray with specified initial capacity. The internal ArrayList is initialized with 
+     *  initialCapacity + ( initialCapacity / 2 ) and filled up with null values for the indexes to initialCapacity.
      *  @param initialCapacity
      */
     public DynArray( int initialCapacity ) {
@@ -82,8 +71,9 @@ public final class DynArray<T> implements Iterable<T> {
     }
     
     /** Sets the value at the specified position of the DynArray and returns the value that was at the position before 
-     *  or null of the position was empty. If the index is out of the range of the current capacity of DynArray, the internal
-     *  ArrayList will be resized to fit the new index. This is the only case where this method will take some more performance.
+     *  or null of the position was empty. If the index is out of the range of the current capacity of DynArray, the 
+     *  internal ArrayList will be resized to fit the new index. This is the only case where this method will take 
+     *  some more performance.
      *  
      * @param index the index to set the value
      * @param value the value to set
