2b2b4af9ca6d1ca2ea832cdc8439c244184446a5
Inari Soft <InariSoft@yahoo.de>
2015-06-23 16:45:38 +0200

    Update ConfigObject.java

--- src/main/java/com/inari/commons/config/ConfigObject.java
+++ src/main/java/com/inari/commons/config/ConfigObject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, Andreas Hefti, InariSoft@yahoo.de 
+ * Copyright (c) 2015, Andreas Hefti, inarisoft@yahoo.de 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
