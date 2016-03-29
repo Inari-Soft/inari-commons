45d8aada4c0539de5e71cdddb45d3d5175491a1d
Inari Soft User <InariSoft@yahoo.de>
2016-03-29 12:10:09 +0200

    Update IEventLog.java

--- src/main/java/com/inari/commons/event/IEventLog.java
+++ src/main/java/com/inari/commons/event/IEventLog.java
@@ -1,7 +1,14 @@
 package com.inari.commons.event;
 
+/** This can be used as a event logging connector interface by connecting it to
+ *  a given IEventDispatcher implementation. The log method gets called on every
+ *  notify call, getting the specified event that is notifed.
+ */
 public interface IEventLog {
     
+    /** Logs the specified Event. Is called on notify event methods of a given IEventDispatcher implementation
+     * @param event The event that is notified
+     */
     void log( Event<?> event );
 
 }
