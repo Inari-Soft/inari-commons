53832d872dcce70b2928b0f5118753ddc4cf82a3
Inari Soft User <InariSoft@yahoo.de>
2016-05-10 11:40:32 +0200

    Update Aspect.java

--- src/main/java/com/inari/commons/lang/aspect/Aspect.java
+++ src/main/java/com/inari/commons/lang/aspect/Aspect.java
@@ -1,7 +1,10 @@
 package com.inari.commons.lang.aspect;
 
+/** This defines a single aspect within a integer id.
+ */ 
 public interface Aspect {
     
+    /** Gets the integer id of the aspect */
     int aspectId();
 
 }
