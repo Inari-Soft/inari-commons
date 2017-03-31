ee0efc3185003eea53150806cdeb3ab94275afb2
andreas hefti <anhefti@yahoo.de>
2017-03-31 10:42:26 +0200

    JavaUtils

--- src/main/java/com/inari/commons/JavaUtils.java
+++ src/main/java/com/inari/commons/JavaUtils.java
@@ -7,8 +7,13 @@ import java.util.Set;
 
 public abstract class JavaUtils {
     
+    /** Use this to create an unmodifiable Set from the given values in the var-arg.
+     *  NOTE: It is the Users concern to care about save varargs on call.
+     * @param values
+     * @return
+     */
     @SafeVarargs
-    public static final <T> Set<T> imutableSet( T... values ) {
+    public static final <T> Set<T> unmodifiableSetSet( T... values ) {
         Set<T> set = new HashSet<T>();
         set.addAll( Arrays.asList( values ) );
         return Collections.unmodifiableSet( set );
