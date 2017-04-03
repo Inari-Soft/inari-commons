8c398c8cff83c6566cc0ca2af2da78107cb2a096
andreas hefti <anhefti@yahoo.de>
2017-04-03 13:04:28 +0200

    more unmodifiable set utils

--- src/main/java/com/inari/commons/JavaUtils.java
+++ src/main/java/com/inari/commons/JavaUtils.java
@@ -1,6 +1,7 @@
 package com.inari.commons;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,5 +19,22 @@ public abstract class JavaUtils {
         set.addAll( Arrays.asList( values ) );
         return Collections.unmodifiableSet( set );
     }
+
+    /** Use this to create an unmodifiable Set from the given Collections in the var-arg.
+     *  NOTE: It is the Users concern to care about save varargs on call.
+     * @param values
+     * @return
+     */
+    @SafeVarargs
+    public static final <T> Set<T> unmodifiableSet( Collection<T>... values ) {
+        Set<T> set = new HashSet<T>();
+        if ( values != null ) {
+            for ( Collection<T> vals : values ) {
+                set.addAll( vals );
+            }
+        }
+        return Collections.unmodifiableSet( set );
+    }
+
 
 }
